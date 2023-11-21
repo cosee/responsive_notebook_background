17faebde8265a69bc15b46571a07c0b92b552fa4
Quoc Huynh <quoc.huynh@cosee.biz>
2023-11-21 09:13:56 +0100

    Set useMaterial3 to false

--- example/lib/theme.dart
+++ example/lib/theme.dart
@@ -4,6 +4,7 @@ class CustomTheme {
   const CustomTheme._();
 
   static ThemeData theme(BuildContext context) => ThemeData(
+        useMaterial3: false,
         colorScheme: Theme.of(context).colorScheme.copyWith(
               primary: green,
               secondary: _yellow,
